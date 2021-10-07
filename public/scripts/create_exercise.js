4171c87391fc6232372190afd007bf41fc9c538a
rayvongregory <rayray31396@gmail.com>
2021-10-06 20:18:40 -0500

    added functions for copying and editing, renamed a few variables, and going to add functions that hide textareas after info is submitted

--- public/scripts/create_exercise.js
+++ public/scripts/create_exercise.js
@@ -1,17 +1,21 @@
 const difficultySelect = document.querySelector("#choose_difficulty")
-const questionAdd = document.querySelector("#question_add")
-const questionDiscard = document.querySelector("#question_discard")
-const answerAdd = document.querySelector("#answer_add")
-const answerDiscard = document.querySelector("#answer_discard")
-const choiceAdd = document.querySelector("#choice_add")
-const choiceDiscard = document.querySelector("#choice_discard")
+const addAllBtn = document.querySelector("#add_all")
+const discardAllBtn = document.querySelector("#discard_all")
+const addAnsBtn = document.querySelector("#answer_add")
+const addQBtn = document.querySelector("#q_add")
+const discardQBtn = document.querySelector("#q_discard")
+const discardAnsBtn = document.querySelector("#answer_discard")
+const addChoiceBtn = document.querySelector("#choice_add")
+const discardChoiceBtn = document.querySelector("#choice_discard")
 const correctAnswerSection = document.querySelector(".correct_answer")
 const questionChoicesSection = document.querySelector(".question_choices")
+const questionBank = document.querySelector(".question_bank")
 const easyQsSection = document.querySelector(".easy_qs")
 const standardQsSection = document.querySelector(".standard_qs")
 const hardQsSection = document.querySelector(".hard_qs")
 const advancedQsSection = document.querySelector(".advanced_qs")
 const noDiffQsSection = document.querySelector(".no_difficulty_qs")
+const noQsYet = document.querySelector(".no_questions_yet")
 const poseQ = document.querySelector("#question")
 const pickDifficulty = document.querySelector("#difficulty")
 const createCorrectAns = document.querySelector("#correct_answer")
@@ -80,7 +84,7 @@ const getRole = async () => {
 //   }
 // }
 
-const addQ = () => {
+const addAll = () => {
   let satisfied = true
   for (let req of publishQuestionReqs) {
     if (req.classList.contains("not_met")) {
@@ -104,7 +108,10 @@ const addQ = () => {
   } while (questions[difficulty][`qid${id}`] !== undefined)
   questions[difficulty][`qid${id}`] = {
     question: questionTextArea.innerHTML,
-    choices,
+    choices: { ...choices },
+  }
+  if (!noQsYet.classList.contains("hide")) {
+    noQsYet.classList.add("hide")
   }
   let p = document.createElement("p")
   question.dataset.id = `qid${id}`
@@ -167,45 +174,97 @@ const addQ = () => {
 
       break
   }
-  discardQ()
+  discardAll()
   discardAns()
   deleteAns()
   deleteChoices()
+  checkForTen()
   resetDifficulty()
-
-  /*
-      <li class="not_met">
-      <span><i class="fas fa-times-circle"></i></span>
-      <p>Add</p>
-      <span role="button" aria-label="Preview question" title="Preview question"
-        ><i class="fas fa-eye"></i
-      ></span>
-      <span role="button" aria-label="Edit question" title="Edit question"
-        ><i class="fas fa-edit"></i
-      ></span>
-      <span
-        role="button"
-        aria-label="Duplicate question"
-        title="Duplicate question"
-        ><i class="fas fa-copy"></i
-      ></span>
-      <span role="button" aria-label="Delete question" title="Delete question"
-        ><i class="fas fa-trash"></i
-      ></span>
-    </li>
-  */
 }
 
-const prevQ = () => {}
-const editQ = () => {}
-const copyQ = () => {}
-const delQ = () => {}
+const prevQ = () => {
+  console.log("previewing question")
+}
+const editQ = (e) => {
+  // get element info
+  let target = e.target.parentElement
+  let targetId = target.dataset.id
+  let targetDiff = target.dataset.difficulty
+  // store info refs in addAllBtn btn
+  addAllBtn.dataset.refId = targetId
+  addAllBtn.dataset.refDiff = targetDiff
+  // change text and labels on add/delete btns
+  addAllBtn.setAttribute("aria-label", "Update question")
+  addAllBtn.setAttribute("title", "Update question")
+  let p = addAllBtn.querySelector("p")
+  p.innerText = "Update Question"
+  discardAllBtn.setAttribute("aria-label", "Discard changes")
+  discardAllBtn.setAttribute("title", "Discard changes")
+  p = discardAllBtn.querySelector("p")
+  p.innerText = "Discard Changes"
+  let q = questions[targetDiff][targetId]
+  // locate info and add to DOM
+  //add question
+  questionTextArea.innerHTML = q.question
+  questionTextArea.dispatchEvent(new KeyboardEvent("keyup"))
+  //set difficulty
+  difficultySelect.value = targetDiff
+  difficultySelect.dispatchEvent(new Event("pointerup"))
+  //add answer
+  if (q.choices.answer) {
+    correctAnswerTextArea.innerHTML = q.choices.answer
+    addAns()
+  }
+  //add choices
+  let keys = Object.keys(q.choices)
+  for (let key of keys) {
+    if (key.startsWith("cid")) {
+      choicesTextArea.innerHTML = q.choices[key]
+      addChoiceBtn.dispatchEvent(new Event("pointerup"))
+    }
+  }
+}
+const copyQ = (e) => {
+  // EZ, let editQ put all info in fields, then call addAll to add it to the bank
+  e.target.previousSibling.dispatchEvent(new Event("pointerup"))
+  addAll()
+}
+const delQ = (e) => {
+  let item = e.target.parentElement
+  let section = item.parentElement
+  let list = section.querySelectorAll("li")
+  if (list.length === 1) {
+    section.classList.add("hide")
+  }
+  let hide = true
+  for (let ul of questionBank.querySelectorAll("ul")) {
+    if (!ul.classList.contains("hide")) {
+      hide = false
+      break
+    }
+  }
+  if (hide) {
+    noQsYet.classList.remove("hide")
+  }
+  let qId = item.dataset.id
+  let qDiff = item.dataset.difficulty
+  delete questions[qDiff][qId]
+  item.remove()
+}
 
-const discardQ = () => {
+const discardAll = () => {
   i = poseQ.querySelector("i")
   questionTextArea.innerHTML = "<p><br></p>"
   poseQ.classList.replace("satisfied", "not_met")
   i.classList.replace("fa-check-circle", "fa-times-circle")
+}
+
+const addQ = () => {
+  console.log("adding q")
+}
+
+const discardQ = () => {
+  onsole.log("discarding q")
 }
 
 const addAns = () => {
@@ -245,8 +304,8 @@ const addAns = () => {
     checkForTen()
   } else {
     choices.answer = correctAnswerTextArea.innerHTML
-    answerAdd.setAttribute("aria-label", "Add answer to question")
-    answerAdd.setAttribute("title", "Add answer to question")
+    addAnsBtn.setAttribute("aria-label", "Add answer to question")
+    addAnsBtn.setAttribute("title", "Add answer to question")
   }
   correctAnswerTextArea.innerHTML = "<p><br></p>"
 }
@@ -257,8 +316,8 @@ const revealAns = () => {
 
 const editAns = (e) => {
   correctAnswerTextArea.innerHTML = choices.answer
-  answerAdd.setAttribute("aria-label", "Update answer")
-  answerAdd.setAttribute("title", "Update answer")
+  addAnsBtn.setAttribute("aria-label", "Update answer")
+  addAnsBtn.setAttribute("title", "Update answer")
 }
 
 const deleteAns = () => {
@@ -341,21 +400,25 @@ const addChoice = (e) => {
   } else {
     choices[ref] = choicesTextArea.innerHTML
     delete e.target.dataset.ref
-    choiceAdd.setAttribute("aria-label", "Add choice to question")
-    choiceAdd.setAttribute("title", "Add choice to question")
+    addChoiceBtn.setAttribute("aria-label", "Add choice to question")
+    addChoiceBtn.setAttribute("title", "Add choice to question")
   }
   choicesTextArea.innerHTML = "<p><br></p>"
 }
 
-const revealChoice = () => {}
+const revealChoice = () => {
+  console.log("revealing choice")
+}
 const editChoice = (e) => {
   let itemID = e.target.parentElement.dataset.id
   choicesTextArea.innerHTML = choices[itemID]
-  choiceAdd.dataset.ref = itemID
-  choiceAdd.setAttribute("aria-label", "Update choice")
-  choiceAdd.setAttribute("title", "Update choice")
+  addChoiceBtn.dataset.ref = itemID
+  addChoiceBtn.setAttribute("aria-label", "Update choice")
+  addChoiceBtn.setAttribute("title", "Update choice")
 }
-const copyChoice = () => {}
+const copyChoice = (e) => {
+  console.log("copying choice")
+}
 const deleteChoice = (e) => {
   let item = e.target.parentElement
   let itemID = item.dataset.id
@@ -378,7 +441,6 @@ const deleteChoices = () => {
     delete choices[choice.dataset.id]
     choice.remove()
   }
-  console.log(choices)
 }
 
 const checkList = (e) => {
@@ -438,12 +500,14 @@ const init = () => {
 }
 
 window.addEventListener("load", init)
-questionAdd.addEventListener("pointerup", addQ)
-questionDiscard.addEventListener("pointerup", discardQ)
-answerAdd.addEventListener("pointerup", addAns)
-answerDiscard.addEventListener("pointerup", discardAns)
-choiceAdd.addEventListener("pointerup", addChoice)
-choiceDiscard.addEventListener("pointerup", discardChoice)
+addAllBtn.addEventListener("pointerup", addAll)
+discardAllBtn.addEventListener("pointerup", discardAll)
+addQBtn.addEventListener("pointerup", addQ)
+discardQBtn.addEventListener("pointerup", discardQ)
+addAnsBtn.addEventListener("pointerup", addAns)
+discardAnsBtn.addEventListener("pointerup", discardAns)
+addChoiceBtn.addEventListener("pointerup", addChoice)
+discardChoiceBtn.addEventListener("pointerup", discardChoice)
 
 // draftBtn.addEventListener("click", draftText)
 // window.addEventListener("beforeunload", compareText)
